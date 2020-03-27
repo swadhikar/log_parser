d396edc59d1ea95a41ed8889068b643305b2ab17
swadhi <cswadhikar@gmail.com>
2020-03-27 17:11:30 +0530

    elastic helper

---
+++ elastic.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# https://www.elastic.co/guide/en/elasticsearch/reference/current/deb.html
+
+wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.6.1-amd64.deb
+wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.6.1-amd64.deb.sha512
+shasum -a 512 -c elasticsearch-7.6.1-amd64.deb.sha512
+sudo dpkg -i elasticsearch-7.6.1-amd64.deb
+
+sudo /bin/systemctl daemon-reload
+sudo /bin/systemctl enable elasticsearch.service
+sudo systemctl start elasticsearch.service
+#sudo systemctl stop elasticsearch.service
---
+++ elastic/elastic_helper.py
@@ -0,0 +1,34 @@
+"""
+curl -X POST "localhost:9200/twitter/_doc/?pretty" -H 'Content-Type: application/json' -d'
+{
+    "user" : "swadhi",
+    "post_date" : "2020-11-15T14:12:12",
+    "message" : "trying out Elasticsearch"
+}
+'
+"""
+from subprocess import Popen, PIPE
+import re
+
+elastic_host = 'localhost'
+elastic_port = 9200
+
+
+def execute_command(command):
+    result = Popen(command, shell=True, stdout=PIPE, stderr=PIPE)
+    result, error = result.communicate()
+    return result.decode('utf-8'), error
+
+
+def create_index(index):
+    query = f'curl -X PUT "{elastic_host}:{elastic_port}/{index}"'
+    result, error = execute_command(query)
+    if re.search('acknowledged.*true', result):
+        print(f'Created index successfully: {index}')
+        return True
+    print(f'Failed to create index: {index}. {error}')
+    return False
+
+
+if __name__ == '__main__':
+    create_index('test123')
